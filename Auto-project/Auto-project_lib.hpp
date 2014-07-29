740a63a740aa0b5bc4a913946583d8bded4db680
oterodr <elena.otero.dr@gmail.com>
2014-07-29 11:21:35 +0200

    resources run over dropbox

--- Auto-project/Auto-project_lib.hpp
+++ Auto-project/Auto-project_lib.hpp
@@ -11,14 +11,17 @@ class IndexDir{
 	// Class that stores the interesting folders for the project.
 private:
 	string _project_folder;  // directory where the project is run. TODO: set using visual studio macros.
+	string _dropbox_folder;  // directory for the project in dropbox.
 	string _images_folder;  // directory for input images.
 	string _output_folder;  // directory to save outputs.
 public:
 	void set_project_folder(string project_folder){ _project_folder = project_folder; };
+	void set_dropbox_folder(string dropbox_folder){ _dropbox_folder = dropbox_folder; };
 	void set_images_folder(string images_folder){ _images_folder = images_folder; };
 	void set_output_folder(string output_folder){ _output_folder = output_folder; };
 
 	string get_project_folder(){ return _project_folder; };
+	string get_dropbox_folder(){ return _dropbox_folder; };
 	string get_images_folder(){ return _images_folder; };
 	string get_output_folder(){ return _output_folder; };
 };
--- Auto-project/run_examples.cpp
+++ Auto-project/run_examples.cpp
@@ -9,8 +9,9 @@ int main()
 	// Create a class to save the input/output folders
 	class IndexDir index_dir;
 
-	index_dir.set_project_folder("C:\Project\code\Auto-project\Auto-project");
-	index_dir.set_images_folder("../../resources/images/");
+	index_dir.set_project_folder("C:/Project/code/Auto-project/Auto-project");
+	index_dir.set_dropbox_folder("C:/Users/EOdR/Dropbox/Auto-project");
+	index_dir.set_images_folder(index_dir.get_dropbox_folder() + "/resources/images/");
 
 	//open_an_image_and_show(index_dir.get_images_folder(), "mandrill.jpg");
 
